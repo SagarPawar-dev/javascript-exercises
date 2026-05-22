274039a17a28f04e06231292cb99b8b2748bed51
sagarpawar3546-cpu <sagarpawar3546@gmail.com>
2026-05-22 23:39:49 +0530

    Complete odin JS exercises 06-11

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    sum=0;
+    let start=Math.min(num1,num2);
+    let end=Math.max(num1,num2);
+    if(num1<0 || num2<0 )
+        return "ERROR";
+    if(!Number.isInteger(num1) || !Number.isInteger(num2)){
+        return "ERROR";
+    }
 
+    else{
+        for(let i=start;i<=end;i++){
+            sum += i;
+        }
+    }    
+    return sum;
 };
 
 // Do not edit below this line
