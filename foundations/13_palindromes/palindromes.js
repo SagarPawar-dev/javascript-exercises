1393c348da795a4fb6cae660a7fe327e51198923
sagarpawar3546-cpu <sagarpawar3546@gmail.com>
2026-06-04 19:46:54 +0530

    complete 12-16 exercise problem

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,24 @@
-const palindromes = function () {
+const palindromes = function (str) {
 
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    
+    cleanedStr = str
+        .toLowerCase()
+        .split('')
+        .filter((character) => alphanumerical.includes(character))
+        .join('');
+
+    reversedStr = cleanedStr
+        .split('')
+        .reverse()
+        .join('');
+    
+    if(cleanedStr === reversedStr){
+        return true;
+    }
+    else{
+        return false;
+    }
 };
 
 // Do not edit below this line
